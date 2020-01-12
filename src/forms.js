333432888483752f8bbff31aec68137f42bd679e
vazasantosh@gmail.com <vazasantosh@gmail.com>
2020-01-12 20:42:44 +0530

    make stuff separate

---
+++ src/forms.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class FormDemo extends React.Component {
+
+    constructor() {
+        
+    }
+
+    render() {
+        return (
+            <form>
+
+            </form>
+        );
+    }
+
+}
